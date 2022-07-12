d5169826e566c9fcc274cd9512b8fb92f4fb6413
Hamid Goudarzi <51421954+HamidGoudarzi1988@users.noreply.github.com>
2022-07-12 13:57:09 -0700

    hero section adjusting

--- app/src/components/hero/Hero.tsx
+++ app/src/components/hero/Hero.tsx
@@ -1,27 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
-import { AppAdmin } from '../../app-admin';
-import Logo from '../../assets/ecad-logo.svg';
+import HeroLogo from '../../assets/Taqueria_icon_hero.svg';
 import Edges from '../edges/Edges';
 import { Button } from '../styles/Button.styled';
 
 const Hero = () => {
 	return (
 		<HeroContainer>
-			<Edges>
-				<HeroConten>
-					<TextCard>
-						<h1>NFT Scaffold is awesome.</h1>
-						<p>
-							Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad,
-							nam no suscipit quaerendum. At nam minimum ponderum.
-						</p>
+			<HeroContentContainer>
+				<Edges>
+					<HeroConten>
+						<img alt='taqueria-logo' src={HeroLogo} />
+						<h1>Taqueria NFT Scaffold</h1>
+						<p>Congratulations on launching your first NFT project on Tezos</p>
 						<Button>Quick Start</Button>
-					</TextCard>
-				</HeroConten>
-			</Edges>
-			{/* <div className={'leftPurpleLine'}></div>
-			<div className={'rightPurpleLine'}></div> */}
+					</HeroConten>
+				</Edges>
+			</HeroContentContainer>
+			<div className='leftPurpleLine' />
+			<div className='rightPurpleLine' />
 		</HeroContainer>
 	);
 };
@@ -29,88 +26,87 @@ const Hero = () => {
 export default Hero;
 
 const HeroContainer = styled.div`
-	background-color: ${({ theme }) => theme.colors.bgLight};
 	width: 100%;
-	padding-top: 20px;
-	padding-bottom: 20px;
-	-webkit-clip-path: polygon(0 0, 100% 0%, 100% 90%, 0% 100%);
-	clip-path: polygon(0 0, 100% 0%, 100% 90%, 0% 100%);
-	position: relative;
-	/* &:before {
+	z-index: -1;
+	&:before {
 		content: '';
 		position: absolute;
-		bottom: -2.5%;
+		bottom: -5%;
 		left: 0;
 		width: 20%;
 		height: 5%;
 		background-color: ${({ theme }) => theme.colors.primary};
 		transform: matrix(1, -0.1, 0, 0.99, 0, 0);
-		z-index: 1;
+		z-index: 2;
+		@media (max-width: 500px) {
+			display: none;
+		}
 	}
 	&:after {
 		content: '';
 		position: absolute;
-		top: 87.5%;
+		bottom: 5%;
 		right: 0;
 		width: 20%;
 		height: 5%;
 		background-color: ${({ theme }) => theme.colors.primary};
 		transform: matrix(1, -0.1, 0, 0.99, 0, 0);
-		z-index: 1;
-	} */
+		z-index: 2;
+		@media (max-width: 500px) {
+			display: none;
+		}
+	}
 	.leftPurpleLine {
 		position: absolute;
-		top: 93.5%;
+		bottom: -1.5%;
 		left: 12.5%;
 		width: 10%;
 		height: 5%;
 		background-color: rgba(160, 102, 170, 0.8);
 		transform: matrix(1, -0.1, 0, 0.99, 0, 0);
-		z-index: 2;
+		z-index: 3;
+		@media (max-width: 500px) {
+			display: none;
+		}
 	}
 	.rightPurpleLine {
 		position: absolute;
-		top: 91.5%;
+		bottom: 1.5%;
 		right: 12.5%;
 		width: 10%;
 		height: 5%;
 		background-color: rgba(160, 102, 170, 0.8);
 		transform: matrix(1, -0.1, 0, 0.99, 0, 0);
-		z-index: 2;
+		z-index: 3;
+		@media (max-width: 500px) {
+			display: none;
+		}
 	}
+`;
+const HeroContentContainer = styled.div`
+	width: 100%;
+	padding-top: 20px;
+	padding-bottom: 20px;
+	background-color: ${({ theme }) => theme.colors.bgLight};
+	-webkit-clip-path: polygon(0 0, 100% 0%, 100% 90%, 0% 100%);
+	clip-path: polygon(0 0, 100% 0%, 100% 90%, 0% 100%);
 `;
 const HeroConten = styled.div`
 	width: 100%;
 	display: flex;
-	flex-direction: row;
-	justify-content: space-between;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 	padding-top: 100px;
 	padding-bottom: 100px;
-`;
+	text-align: center;
+	img {
+		width: 200px;
+		height: 200px;
+	}
 
-const TextCard = styled.div`
-	width: 50%;
-	display: flex;
-	flex-direction: column;
-	align-items: flex-start;
-	justify-content: center;
-	h1 {
-		max-width: 400px;
-		padding-bottom: 20px;
-	}
 	p {
-		max-width: 380px;
+		max-width: 280px;
 		padding-bottom: 20px;
 	}
-	@media (max-width: ${({ theme }) => theme.breakpoints.small}) {
-		align-items: center;
-		width: 100%;
-	}
-`;
-const ButtonContainer = styled.div`
-	display: flex;
-	justify-content: flex-end;
-	align-items: center;
-	width: 50%;
-	margin-left: auto;
 `;
